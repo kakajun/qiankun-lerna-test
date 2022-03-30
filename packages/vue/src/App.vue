266d30c32a6c3d1face4427baba6ecb78686a247
马军 <253495832@qq.com>
2022-03-30 22:23:24 +0800

    fix: 1

--- packages/vue/src/App.vue
+++ packages/vue/src/App.vue
@@ -6,7 +6,7 @@
       |
       <router-link to="/about">About</router-link>
        |
-      <router-link to="/about1">About1</router-link>
+      <!-- <router-link to="/about1">About1</router-link> -->
     </div>
     <router-view />
   </div>
